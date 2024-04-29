2bba0a62aadc0fa3f88c2ac363b3a4836ebc814b
Chris Cheape <chris@cheape.cc>
2024-04-28 23:48:44 -0400

    reorg scripts

---
+++ src/main.js
@@ -0,0 +1,55 @@
+function main() {
+  $(window).load(function () {
+    layout.sizeMap($("input[name='mapAlign']:checked").val()); //arranges the maps to best fit on screen
+    map.populate(); //adds dungeons and chest icons to the maps
+    settings.applyCookie(); //applies saved user settings, or defaults if no cookie found
+    stats.fetch();
+    loadState();
+  });
+
+  $(window).resize(function () {
+    layout.sizeMap($("input[name='mapAlign']:checked").val()); //re-arranges the maps if window size changes
+  });
+
+  $('.icon,input').hover(
+    function () {
+      $(this).attr('tooltip-data', $(this).attr('title'));
+      $(this).removeAttr('title');
+      $('#caption').html($(this).attr('tooltip-data'));
+    },
+    function () {
+      $(this).attr('title', $(this).attr('tooltip-data'));
+      $(this).removeAttr('tooltip-data');
+      $('#caption').html('');
+    }
+  );
+
+  $('.icon').hover(
+    function () {
+      stats.find(this);
+    },
+    function () {
+      if (settings.predictor == 0) {
+        stats.clear();
+      } else {
+        stats.find('boss10');
+      }
+    }
+  );
+
+  $('.icon').mousedown(function (event) {
+    //adds listener for right-click on icons
+    if (event.which == 3) {
+      toggle.icon(this, true);
+    }
+  });
+
+  $('#timer').mousedown(function (event) {
+    //adds listener for right-click on icons
+    if (event.which == 3) {
+      resetTimer();
+    }
+  });
+}
+
+main();
